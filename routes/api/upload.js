3682d40d2cc4fec79f016eebf78624ce6c0d31df
Rlss91 <rlss91@gmail.com>
2022-11-09 15:57:29 +0200

    uploads

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const multer = require("../../config/multerTypes");
+const upload = multer.createMulter(
+  "uploads/",
+  3000000,
+  multer.allowedTypes.img
+);
 
 router.post("/", upload.single("avatar"), function (req, res) {
   // req.file is the `avatar` file
